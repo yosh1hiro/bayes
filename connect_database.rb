ea2787137bc65f80dc159bc2ee34849b3c22a0d2
yoshihiro asada <yoshihiro0921@gmail.com>
2015-09-17 11:46:57 +0900

    Add to output conditinal probability on csv

--- connect_database.rb
+++ connect_database.rb
@@ -5,12 +5,22 @@ require "sequel"
 
 module ConnectDatabase
   DB = Sequel.connect('mysql2://intern:livesense@10.26.2.77/jobtalk_dump')
-  def gets_libel
-    DB[:comments].where(libel:1).order{id.asc}.limit(400).select(:comment).map(&:values).flatten
+
+  module_function
+
+  def gets_libel_learning(limit, offset)
+    DB[:comments].where(libel:1).order{id.desc}.limit(limit, offset).select(:comment).map(&:values)
   end
-  module_function :gets_libel
-  def gets_approval
-    DB[:comments].where(approved:1, member_career_post_id:1297552..1379231).order{id.asc}.limit(400).select(:comment).map(&:values).flatten
+
+  def gets_approval_learning(limit, offset)
+    DB[:comments].where(approved:1, member_career_post_id:1297552..1379231).order{id.desc}.limit(limit, offset).select(:comment).map(&:values)
   end
-  module_function :gets_approval
+
+  def gets_libel_test
+    DB[:comments].where(libel:1).order{id.asc}.limit(400).select(:comment).map(&:values)
+  end
+
+  def gets_approval_test
+    DB[:comments].where(approved:1, member_career_post_id:1297552..1379231).order{id.asc}.limit(400).select(:comment).map(&:values)
+  end
 end
